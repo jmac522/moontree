383c26d52d6c2ccc5fb33e596dd89b5eb71fe5b2
jmac522 <johnmccarthy@John-McCarthys-MacBook-Pro-2.local>
2015-08-14 14:01:50 -0400

    instagram API

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
   def index
+    @instagram = Instagram.user_recent_media("moontreestudio", {:count => 1})
   end
 end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+require "instagram"
+
+Instagram.configure do |config|
+  config.client_id = "60ca8702cfef49d393a04b9b15ff1f10"
+  config.access_token = "6334ad35b1e34cdd8fd65cb6a9ae9a34"
+end
